c5abb6f94e72d049b6165dd9b524c55351dc8e77
MUPPALLA2003 <muppallakomaliitkgp2022@gmail.com>
2026-06-18 22:15:38 +0530

    bugs

--- src/model/Attention.py
+++ src/model/Attention.py
@@ -6,6 +6,7 @@ import math
 class CausalSelfAttention(nn.Module):
 
     def __init__(self,n_embd,n_head,seq_len,dropout):
+        
         super().__init__()
 
         assert n_embd % n_head == 0
@@ -14,19 +15,23 @@ class CausalSelfAttention(nn.Module):
         self.n_embd = n_embd
         self.dropout = nn.Dropout(dropout)
         self.attn_matrix = nn.Linear(n_embd,3*n_embd)
+        self.out_proj = nn.Linear(n_embd, n_embd)
         self.flash = hasattr(torch.nn.functional,'scaled_dot_product_attention')
         self.seq_len = seq_len
 
         if not self.flash:
+
             print('flash attention requires PyTorch >= 2.0')
             self.register_buffer('mask',torch.tril(torch.ones(seq_len,seq_len)).view(1,1,seq_len,seq_len))
     
     def attention(self,q,k,v,T):
 
         if self.flash:
+
             y = F.scaled_dot_product_attention(q,k,v,attn_mask = None,dropout_p = self.dropout.p if self.training else 0,is_causal = True)
 
         else:
+
             attention_logits = (q @ k.transpose(-2,-1)) * (1.0/math.sqrt(k.size(-1)))
             attention_logits = attention_logits.masked_fill(self.mask[:,:,:T,:T] == 0,float('-inf'))
             attention_scores = F.softmax(attention_logits,dim = -1)
@@ -45,6 +50,6 @@ class CausalSelfAttention(nn.Module):
         v = value.view(B,T,self.n_head,self.n_embd//self.n_head).transpose(1,2)
         y = self.attention(q,k,v,T)
         y = y.transpose(1,2).contiguous().view(B,T,C)
-        y = self.dropout(y)
+        y = self.dropout(self.out_proj(y))
 
         return y
